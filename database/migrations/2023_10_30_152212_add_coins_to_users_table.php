6d3ff8222eed5ef489f3a67122ddbc80364895e3
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-30 21:26:07 +0600

    add coins to users table

--- database/migrations/2023_10_30_152212_add_coins_to_users_table.php
+++ database/migrations/2023_10_30_152212_add_coins_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->double('coins', 10, 2)->nullable()->default(0)->after('password');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('coins');
         });
     }
 };
